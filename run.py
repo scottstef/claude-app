14834c68e68f9d9b43033638584e941f082d4987
Scott Stefanoski <scottstefanoski@gmail.com>
2025-05-11 12:15:55 -0400

    needed to add sig handler for shutdown

--- run.py
+++ run.py
@@ -2,8 +2,24 @@
 from app import create_app
 from app.config import Config
 import os
+import signal
+import sys  
+
+def signal_handler(sig, frame):
+    print('Received signal to shut down. Shutting down gracefully...')
+    sys.exit(0)
+
+
+signal.signal(signal.SIGTERM, signal_handler)  # Termination signal
+signal.signal(signal.SIGINT, signal_handler)   # Interrupt signal
 
 app = create_app()
+
+@app.before_request
+def log_request():
+    from flask import request
+    import logging
+    logging.info(f"Received request: {request.method} {request.path}")
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
